f56667c4e5aa12850611245f7a9ece9dae250b00
邓松权 <974170221@qq.com>
2020-06-27 07:40:17 +0800

    父子组件传值

---
+++ passval/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <hello-world :todos="todos">
+    <template slot-scope="slotProps">
+      <span v-if="slotProps.todo.isComplete"></span>
+      <span>{{slotProps.todo.text}}</span>
+    </template>
+  </hello-world>
+  <hello-world :msg="msg" v-model="msg" />
+
+</template>
+
+<script>
+
+import HelloWorld from "./components/HelloWorld";
+export default {
+  name: 'App',
+  components: {
+      HelloWorld
+  },
+    data(){
+      return{
+          msg:'',
+         todos:[
+             {
+                 id:0,
+                 text:'raily0',
+                 isComplete: false
+             },
+             {
+                 text:'raily01',
+                 id:1,
+                 isComplete: true
+             },
+             {
+                 text:'raily02',
+                 id:2,
+                 isComplete: false
+             },
+             {
+                 text:'raily03',
+                 id:3,
+                 isComplete: false
+             }
+         ]
+      }
+    },
+    methods:{
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ passval/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            <slot :todo="todo">
+
+            </slot>
+        </li>
+    </ul>
+    <input type="text" :value="msg" @input="$emit('input',$event.target.value)">
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg:'',
+            todos:{
+                type: Array
+            }
+        },
+        data(){
+            return{
+                sonValue: this.fatherValue
+            }
+        },
+        watch:{
+            sonValue(newValue, oldValue){
+                this.$emit('update:fatherValue', newValue);
+            },
+            fatherValue(newValue){
+                this.sonValue = newValue;
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
